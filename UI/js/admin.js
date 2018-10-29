e679ceb01f09ba626bed1e8c08a25a475403be2a
Oyedeji Peace <42093785+oyedejipeace@users.noreply.github.com>
2018-10-29 13:18:09 -0300

    Initial commit

---
+++ UI/js/admin.js
@@ -0,0 +1,21 @@
+// var editButton = document.getElementById('editButton');
+var address = document.getElementsByClassName('address');
+var presentLocation = document.getElementsByClassName('presentLocation');
+var newLocation = document.getElementsByClassName('newLocation');
+var editButton2 = document.getElementsByClassName('editButton2');
+
+
+// (function () {
+//     "use strict";
+//     var buttons = document.getElementsByClassName('editButton');
+//     for ( var i in Object.keys( buttons ) ) {
+//         buttons[i].onclick = function() {
+//             var k;
+//                     for (k =0; k < presentLocation.length; k++) {
+//                         presentLocation[k].style.display = "none";
+//                         newLocation[k].style.display = "block";
+//                     };
+//             console.log(this.id);
+//         };
+//     }
+// })();
