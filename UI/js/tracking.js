921c1f193bcef3fc7d2bb5e2638f734900344025
oyedejipeace <oyedejipeace@yahoo.com>
2018-11-16 06:14:04 -0400

    Modify Ui templates - Refactored all Ui pages as directed by LFA

---
+++ UI/js/tracking.js
@@ -0,0 +1,23 @@
+// var address = document.getElementById('address');
+// var previewAddressTo = document.getElementById('previewAddressTo');
+// var editButton = document.getElementById('editButton');
+// var cancelButton = document.getElementById('cancelButton');
+// var submitButton = document.getElementById('submitButton');
+
+// When the user clicks outside of the password field, hide the message box
+document.getElementById('editButton').addEventListener('click', function() {
+    //     address.style.display = "block";
+    //     previewAddressTo.style.display = "none";
+    //     submitButton.style.display = "block";
+    //     cancelButton.style.display = "none";
+         document.getElementById('editButton').style.display = "none"
+              break
+    });
+    
+    
+    // submitButton.addEventListener('click', function() {
+    //     address.style.display = "none";
+    //     submitButton.style.display = "none";
+    //     previewAddressTo.style.display = "block";
+    //     cancelButton.style.display = "block";
+    // });
