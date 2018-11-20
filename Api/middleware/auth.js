4b1e685b589412e9aafce9b6911376496bb56275
oyedejipeace <oyedejipeace@yahoo.com>
2018-11-20 00:53:00 -0400

    Refactor  middleware validation - Added validator.js library for full coverage of middeware validation - Added validation for emails, passwords and input strength - it prevents fictituous input data from being parsed to the database by aborting every instance there is error in the API queries
    [Finishes #162077890]

--- Api/middleware/auth.js
+++ Api/middleware/auth.js
@@ -1,25 +1,25 @@
-import  jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export async function verifyUserToken(req, res, next) {
   const token = req.headers['x-access-token'];
-  if (!token)
-    return res.status(403).send({ auth: "false", message: "No token provided" });
-  await jwt.verify(token, process.env.User_Secret, function(err, decoded) {
-    if (err){return res.status(500).send({ auth: "false", message: "Failed to authenticate token" });}
-    // update body with the Id 
-    req.body.user_id = decoded.user_id;
-    req.body.user_role = decoded.user_role;
-    req.body.user_email = decoded.user_email;
-    req.body.user_name = decoded.user_name;
-    next();
+  if (!token) { return res.status(403).send({ auth: 'false', message: 'No token provided' }); }
+  await jwt.verify(token, process.env.User_Secret, (err, decoded) => {
+    if (err) { return res.status(500).send({ auth: 'false', message: 'Failed to authenticate token' }); }
+    // update body with the Id
+    req.body.userId = decoded.userId;
+    req.body.userRole = decoded.userRole;
+    req.body.userEmail = decoded.userEmail;
+    req.body.userName = decoded.userName;
+    return next();
   });
 }
 
-export function verifyAdmin(req, res, next){
-if (req.body.user_role === "User"){
-  return res.status(401).send({success: "false", Message: "Forbidden Route, User not Authorised"})
+export function verifyAdmin(req, res, next) {
+  if (req.body.userRole === 'User') {
+    return res.status(401).send({ success: 'false', Message: 'Forbidden Route, User not Authorised' });
+  }
+  return next();
 }
-next();
-} 
--- Api/middleware/usersValidation.js
+++ Api/middleware/usersValidation.js
@@ -1,38 +1,97 @@
+import Validator from 'validator';
 /**
 * Validate post and Put request contains all required parameters
 * @param {*} req - incomming API Request
 * @param {*} res - response to the validity of the data
 */
-function userValidation(req, res, next) {
-    let error = {};
-    if (!(req.body.user_name)) { error.UserName_Error = 'User_name is Required'; }
-    if (!(req.body.user_email)) { error.Email_Error = 'User_email is Required'; }
-    if (!(req.body.user_password)) { error.Password_Error = 'Password is Required'; }
-    if ((Object.keys(error).length) > 0) { return res.status(400).send({ status: 'Bad Request', success: "false", Error_Log: error }); }
-    next();
-}
+export const userValidation = (req, res, next) => {
+  const {
+    userName, userEmail, userPassword,
+  } = req.body;
+  const error = {};
+  if (Validator.isEmpty(userName.trim()) === '') {
+    error.userName = 'userName is Required';
+  }
+  if (!Validator.isAlphanumeric(userName)) {
+    error.userName = 'Only alphabets and numbers are allowed.';
+  }
+  if (typeof userEmail !== 'string' || Validator.isEmpty(userEmail.trim()) === '') {
+    error.userEmail = 'Email is Required';
+  }
+  if (!userPassword) {
+    error.userPassword = 'Password is Required';
+  }
+  if ((Object.keys(error).length) > 0) { return res.status(400).send({ status: 'Bad Request', success: 'false', Error_Log: error }); }
+  return next();
+};
 
+export const userLogin = (req, res, next) => {
+  const {
+    userEmail, userPassword,
+  } = req.body;
+  const error = {};
+  if (typeof userEmail !== 'string' || Validator.isEmpty(userEmail.trim()) === '') {
+    error.userEmail = 'Email is Required';
+  }
+  if (!userPassword) {
+    error.userPassword = 'Password is Required';
+  }
+  if ((Object.keys(error).length) > 0) { return res.status(400).send({ status: 'Bad Request', success: 'false', Error_Log: error }); }
+  return next();
+};
+
+// Check for Lenght
+export const userLength = (req, res, next) => {
+  const {
+    userName, userEmail, userPassword,
+  } = req.body;
+  const error = {};
+  if (!Validator.isLength(userName, { min: 3, max: 30 })) {
+    error.userName = 'name should be between 3 to 30 characters';
+  }
+  if (!Validator.isLength(userEmail, { min: 10, max: 50 })) {
+    error.userEmail = 'email must be between 10 to 50 characters';
+  }
+  if (!Validator.isLength(userPassword, { min: 8, max: 50 })) {
+    error.userPassword = 'Passowrd of sender should be between 8 to 50 characters';
+  }
+  if ((Object.keys(error).length) > 0) return res.status(400).json({ status: 'Bad Request', success: 'false', Error_Log: error });
+  return next();
+};
 /**
-* Validate post and Put request contains a valid interger ID
+* Validate email contains a valid
 * @param {*} req - incomming API request
 * @param {*} res - response to the request Validity
 */
-function userIdValidation(req, res, next) {
-    let error = {};
-    const id = req.params.userId;
-    if (id === undefined || id === null || id === "") { error.userId_Error = 'Bad Request, User ID is Required'; }
-    else { if (!(Number.isInteger(parseInt(id))===true)) { error.status = 'unsuccessful'; error.userId_Error = 'Bad Request, User ID is not Valid'; } }
-    if ((Object.keys(error).length) > 0) { return res.status(400).send({ status: "unsuccessful", Error_Log: error }); }
-    next();
-}
+export const emailvalidation = (req, res, next) => {
+  const { userEmail } = req.body;
+  const error = {};
+  if (!Validator.isEmail(userEmail)) {
+    error.userEmail = 'Email of parcel Sender is invalid';
+  }
+  if ((Object.keys(error).length) > 0) return res.status(400).json({ status: 'Bad Request', success: 'false', Error_Log: error });
+  return next();
+};
 
-function userPostIdValidation(req, res, next) {
-    return res.status(400).send({ status: "unsuccessful", Error: "Bad Request, User ID is not Required" });
-}
+/**
+* Validate post and Put request contains a valid integer ID
+* @param {*} req - incomming API request
+* @param {*} res - response to the request Validity
+*/
+export const userIdValidation = (req, res, next) => {
+  const { userId } = req.params;
+  const error = {};
+  if (!Number.isInteger(parseInt(userId, 10))) {
+    error.userId = 'The Id must be a number';
+  }
+  if (userId === undefined || userId === null || userId === '') {
+    error.userId = 'User id  is required';
+  }
 
-//export statement
-export default {
-    Validation: userValidation,
-    userIdValidation: userIdValidation,
-    PostIdValidation: userPostIdValidation
+  if ((Object.keys(error).length) > 0) { return res.status(400).send({ status: 'unsuccessful', Error_Log: error }); }
+  return next();
+};
+
+export const userPostIdValidation = (req, res) => {
+  res.status(400).send({ status: 'unsuccessful', Error: 'Bad Request, User ID is not Required' });
 };
