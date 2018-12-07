311ebe0c34b04ebff59e5ff8d31d4223fc430d33
oyedejipeace <oyedejipeace@yahoo.com>
2018-12-07 02:48:26 -0400

    feat(fetch) create dist1 folder for transpiled codes - Transpiled codes using  @babel/polyfill to allow for fetch syntax in unsupporting browsers

---
+++ client/public/dist1/controller.js
@@ -0,0 +1,46 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+class Controller {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async get(getRoute) {
+    this.getUrl = this.baseUrl + getRoute;
+    await fetch(this.getUrl).then(res => res.json()).then(data => {
+      this.data = data;
+    });
+    return this.data;
+  }
+
+  async post(postRoute, method) {
+    this.postUrl = this.baseUrl + postRoute;
+    this.method = method;
+    await fetch(this.postUrl, method).then(res => res.json()).then(data => {
+      this.data = data;
+    });
+    return this.data;
+  }
+
+  async put(putRoute, method) {
+    this.puUrl = this.baseUrl + putRoute;
+    this.method = method;
+    await fetch(this.putUrl, method).then(res => res.json()).then(data => {
+      this.data = data;
+    });
+    return this.data;
+  }
+
+  async delete(deleteRoute, method) {
+    this.deleteUrl = this.baseUrl + deleteRoute;
+    this.method = method;
+    await fetch(this.deleteUrl, method).then(res => res.json()).then(data => {
+      this.data = data;
+    });
+    return this.data;
+  }
+}
+exports.default = Controller;
